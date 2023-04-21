d723e164607b5bce8759df8f3ade6a6e71c02f7a
osmanyurdakul <34229250+osmanyurdakul@users.noreply.github.com>
2023-04-21 19:03:00 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,33 @@
+#include "hila.h"
+#include <stdbool.h>
+
+extern volatile bool hila_cmd_isReceived;
+extern volatile bool diag_cmd_isReceived;
+extern volatile bool test_cmd_isReceived;
+
+int main(void)
+{
+    hila_initialize();
+    
+    while(1)
+    {
+
+        if (hila_cmd_isReceived == true)
+        {
+            hila_rs485_cmd_execute();
+            hila_cmd_isReceived = false;
+        }
+
+        if (diag_cmd_isReceived == true)
+        {
+            hila_diag_cmd_execute();
+            diag_cmd_isReceived = false;
+        }
+
+        if (test_cmd_isReceived == true)
+        {
+            hila_test_cmd_execute();
+            test_cmd_isReceived = false;
+        }
+    }
+}
